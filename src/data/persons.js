bbdb0b4002ab733b7cb83425e02b18407cb179ff
Anders Lykkestrup <anders.lykkestrup@gmail.com>
2019-03-05 13:05:02 +0100

    Axel og Oliver på. Flex til grid på Who we are

--- src/data/persons.js
+++ src/data/persons.js
@@ -1,46 +1,60 @@
-import Anders from "../images/BillederAfOs/Portrait-of-Anders-min.jpg";
-import Benjamin from "../images/BillederAfOs/Portrait-of-Benjamin-min.jpg";
-import Emil from "../images/BillederAfOs/Portrait-of-Emil-min.jpg";
-import Sigurd from "../images/BillederAfOs/Portrait-of-Sigurd-min.jpg";
-import Valentin from "../images/BillederAfOs/Portrait-of-Valentin-min.jpg";
-import Zacharias from "../images/BillederAfOs/Portrait-of-Zach-min.jpg";
+import Anders from '../images/BillederAfOs/Portrait-of-Anders-min.jpg'
+import Benjamin from '../images/BillederAfOs/Portrait-of-Benjamin-min.jpg'
+import Emil from '../images/BillederAfOs/Portrait-of-Emil-min.jpg'
+import Sigurd from '../images/BillederAfOs/Portrait-of-Sigurd-min.jpg'
+import Valentin from '../images/BillederAfOs/Portrait-of-Valentin-min.jpg'
+import Zacharias from '../images/BillederAfOs/Portrait-of-Zach-min.jpg'
+import Oliver from '../images/BillederAfOs/Portrait-of-Oliver-min.jpg'
+import Axel from '../images/BillederAfOs/Portrait-of-Axel-min.jpg'
 const persons = [
   {
-    name: "Sigurd Carlsen",
-    titel: "Sigurd.titel",
+    name: 'Sigurd Carlsen',
+    titel: 'Sigurd.titel',
     face: Sigurd,
-    text: "Sigurd.text"
+    text: 'Sigurd.text',
   },
   {
-    name: "Emil Johansen",
-    titel: "Emil.titel",
+    name: 'Emil Johansen',
+    titel: 'Emil.titel',
     face: Emil,
-    text: "Emil.text"
+    text: 'Emil.text',
   },
   {
-    name: "Valentin Rosenberg Larsen",
-    titel: "Valentin.titel",
+    name: 'Valentin Rosenberg Larsen',
+    titel: 'Valentin.titel',
     face: Valentin,
-    text: "Valentin.text"
+    text: 'Valentin.text',
   },
   {
-    name: "Zacharias Dyna Knudsen",
-    titel: "Zacharias.titel",
+    name: 'Zacharias Dyna Knudsen',
+    titel: 'Zacharias.titel',
     face: Zacharias,
-    text: "Zacharias.text"
+    text: 'Zacharias.text',
   },
   {
-    name: "Benjamin Legarth",
-    titel: "Benjamin.titel",
+    name: 'Oliver Boda',
+    titel: 'Oliver.titel',
+    face: Oliver,
+    text: 'Oliver.text',
+  },
+  {
+    name: 'Benjamin Legarth',
+    titel: 'Benjamin.titel',
     face: Benjamin,
-    text: "Benjamin.text"
+    text: 'Benjamin.text',
   },
   {
-    name: "Anders Lykkestrup",
-    titel: "Anders.titel",
+    name: 'Anders Lykkestrup',
+    titel: 'Anders.titel',
     face: Anders,
-    text: "Anders.text"
-  }
-];
+    text: 'Anders.text',
+  },
+  {
+    name: 'Axel Christfort',
+    titel: 'Axel.titel',
+    face: Axel,
+    text: 'Axel.text',
+  },
+]
 
-export default persons;
+export default persons
